aa3b97bca9f7d83a67a879fb4159fe571c7335cf
Zac Uslianer <zacuslianer@gmail.com>
2020-02-03 03:27:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -13,9 +13,9 @@ def find_max_value(array)
   x = array[0]
   array.length.times do |index|
     if array[index] > x
-      return array
+      x = array[index]
   end
-   
+   x
 end
 
 def find_min_value(array)
