8e2b2bbd19390d8cd55a12a4683f83cd1f08de43
Zac Uslianer <zacuslianer@gmail.com>
2020-02-03 03:26:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.length.times do |count|
+  array.length.times do |index|
     if array[count] == array.max()
       return array
   end
